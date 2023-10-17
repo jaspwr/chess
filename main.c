1831ac11d82a50ab0604628ac108276dc0cb02d9
Jasper <40232406+jaspwr@users.noreply.github.com>
2023-10-17 23:24:12 +1100

    Magic bitboards

--- main.c
+++ main.c
@@ -56,6 +56,8 @@ typedef struct {
     int seen_position_hashes_count;
 } GameState;
 
+void update_game_state_info(GameState* gs);
+
 typedef enum {
     P_PAWN,
     P_KNIGHT,
@@ -109,6 +111,11 @@ typedef struct {
     Move move;
     float eval;
 } EvaluatedMove;
+
+typedef struct {
+    int x;
+    int y;
+} PosOffset;
 
 const Board W_CASTLE_MASK_K = 0x60;
 const Board W_CASTLE_MASK_Q = 0xe;
@@ -500,6 +507,8 @@ PawnMoves pawn_legal_moves(Position pos, GameState* gs) {
 
     moves &= ~(gs->side[S_WHITE].piece[P_KING] | gs->side[S_BLACK].piece[P_KING]);
 
+    moves &= ~gs->side[gs->turn].all_pieces;
+
     PawnMoves pm = { 0 };
     pm.all_moves = moves;
     pm.en_passant_moves = en_passant_moves;
@@ -507,8 +516,155 @@ PawnMoves pawn_legal_moves(Position pos, GameState* gs) {
     return pm;
 }
 
+static Board COLS[8];
+static Board ROWS[8];
+
 static Board KNIGHT_MOVES_LUT[64];
 static Board KING_MOVES_LUT[64];
+static Board BISHOP_MOVES_LUT[64];
+static Board ROOK_MOVES_LUT[64];
+
+#define ROOK_PERMS 8 * 4096
+#define ROOK_SHIFT 15
+#define BISHOP_PERMS 8 * 4096
+#define BISHOP_SHIFT 15
+
+static Board BISHOP_MAGIC_BOARDS[64][BISHOP_PERMS] = { 0 }; // 256 K
+static Board ROOK_MAGIC_BOARDS[64][ROOK_PERMS] = { 0 }; // 2048 K
+
+static u64 BISHOP_MAGIC_NUMBERS[64] = {
+    2658911450287794421,
+    6852417887120316938,
+    1159619772494454267,
+    4530306807827729113,
+    4184186518776449728,
+    6996729517409051523,
+    6694277110281440227,
+    4367046700827347423,
+    5002521922874173148,
+    707789975046747826,
+    8845803124310741094,
+    3053780606037994088,
+    782875984623577188,
+    1340588251091286675,
+    5117580335483048948,
+    8042612948621996202,
+    2783607372402123321,
+    1442455459770969771,
+    5692530968041345214,
+    4649229338657423104,
+    2305795996909174787,
+    4611698444337271150,
+    4629462408027598048,
+    8622995163449815274,
+    6456489114168196145,
+    510080667540968688,
+    4323454183829978828,
+    68334489678698,
+    3558652225865219584,
+    5706513065946585703,
+    1786281091850907230,
+    8201950827265312549,
+    694435915564839760,
+    840916925791039369,
+    1115610479759747424,
+    6880091598734649088,
+    9161611439159255552,
+    5414701498144979799,
+    3359132947749940723,
+    2167603366743129587,
+    2022505955381231761,
+    5998988709168406901,
+    3084769873457330128,
+    7562712105529095721,
+    2634012166121824780,
+    8481960309935617736,
+    1295637020739438321,
+    7389596876633812746,
+    4662164641528356395,
+    4274287365172863505,
+    2646470775556534080,
+    825937101719960739,
+    3176643519900238103,
+    9026745600027081100,
+    2125726254840799092,
+    7498071283202784097,
+    3289870684522899707,
+    5005907598103146988,
+    7827854030464587121,
+    2189585998322548473,
+    8686252344764419816,
+    2440490461859161454,
+    152347237491388123,
+    8496541520937575975,
+};
+
+static u64 ROOK_MAGIC_NUMBERS[64] = {
+    351979748819113472,
+    7803309538379299328,
+    1736271716617851776,
+    8109988271961577632,
+    432345414004662826,
+    6673893786975006608,
+    4474708985453114624,
+    8141247327844093598,
+    7425962628245966592,
+    1642867420846126720,
+    2759411911526057344,
+    3357996469423967328,
+    4521686761654498272,
+    5079380718783458248,
+    8005694169337838520,
+    5553027955233797434,
+    4089864033709443328,
+    5160404229161359616,
+    4389565480264011648,
+    6154169681462285184,
+    6751583036203832889,
+    7784001533825787536,
+    7611136035526543800,
+    4887443686719849878,
+    6633336694756633856,
+    3595239369322866432,
+    7044805029705048064,
+    2281069068459920160,
+    4379372414207810190,
+    3273077888100893176,
+    6836300768995319696,
+    6074204129578170796,
+    6511605973395510272,
+    188959452880595840,
+    9223320189913268096,
+    5470114901861073408,
+    174381527388389152,
+    2842330991730432984,
+    5162016821703487056,
+    6755246923111856388,
+    3917853114319556096,
+    5621910873506324480,
+    7574010561759584256,
+    7020725938020357888,
+    8045912710459918336,
+    1679284573738097840,
+    8085835441630863108,
+    1723801058847070596,
+    1644622125664786944,
+    5076042003988832512,
+    6561642211069593856,
+    4907242536322792960,
+    1066234342103656960,
+    5723721981445960192,
+    7032640680205726208,
+    5945373557256219552,
+    4643755003622426662,
+    4725706427850877098,
+    1489248111780252278,
+    2766412166700150694,
+    8690697145264131078,
+    4008559275060228042,
+    191737606960205964,
+    6096466831532416022,
+};
 
 #define add_move(x, y) position_offset(pos, x, y, &move_list)
 
@@ -550,6 +706,50 @@ void generate_king_LUT() {
     }
 }
 
+void generate_rook_LUT() {
+    for (Position pos = 0; pos < 64; pos++) {
+        int row = row_from_pos(pos);
+        int col = col_from_pos(pos);
+        ROOK_MOVES_LUT[pos] = (ROWS[row] | COLS[col]) & ~pos_to_bitboard(pos);
+    }
+}
+
+void generate_bishop_attack_bitboard(Position pos) {
+    BISHOP_MOVES_LUT[pos] = 0;
+
+    const PosOffset offsets[4] = {
+        { 1, 1 },
+        { -1, 1 },
+        { 1, -1 },
+        { -1, -1 },
+    };
+
+    int row = row_from_pos(pos);
+    int col = col_from_pos(pos);
+
+    for (int dir = 0; dir < 4; dir++) {
+        for (int dist = 1; dist < 8; dist++) {
+            int row_ = row + offsets[dir].y * dist;
+            int col_ = col + offsets[dir].x * dist;
+
+            if (row_ < 0 || row_ > 7 || col_ < 0 || col_ > 7) {
+                break;
+            }
+
+            Position pos_ = pos_from_row_and_col(row_, col_);
+            BISHOP_MOVES_LUT[pos] |= pos_to_bitboard(pos_);
+        }
+    }
+}
+
+void generate_bishop_LUT() {
+    for (Position pos = 0; pos < 64; pos++) {
+        generate_bishop_attack_bitboard(pos);
+
+        assert(BISHOP_MOVES_LUT[pos] != 0);
+    }
+}
+
 Board knight_attacks(Position pos, GameState* gs) {
     assert_piece_exists(P_KNIGHT, pos, gs);
 
@@ -588,12 +788,9 @@ Board king_attacks(Position pos, GameState* gs) {
     return moves;
 }
 
-typedef struct {
-    int x;
-    int y;
-} PosOffset;
+Board sliding_piece_attacks(Position pos, Board occupancies,
+    const PosOffset* offsets, int offset_dirs_count) {
 
-Board sliding_piece_attacks(Position pos, GameState* gs, const PosOffset* offsets, int offset_dirs_count) {
     Position moves[128] = { 0 };
     PositionList move_list = { 0 };
     move_list.positions = moves;
@@ -614,16 +811,22 @@ Board sliding_piece_attacks(Position pos, GameState* gs, const PosOffset* offset
 
             Position p = move_list.positions[move_list.length - 1];
 
-            Board pieces = gs->all_pieces &
-                ~gs->side[!gs->turn].piece[P_KING];
-
-            if (pieces & pos_to_bitboard(p)) {
+            if (occupancies & pos_to_bitboard(p)) {
                 break;
             }
         }
     }
 
     return position_list_to_bitboard(&move_list);
+}
+
+Board sliding_piece_attacks_from_gs(Position pos, GameState* gs,
+    const PosOffset* offsets, int offset_dirs_count) {
+
+    Board occupancies = gs->all_pieces &
+        ~gs->side[!gs->turn].piece[P_KING];
+
+    return sliding_piece_attacks(pos, occupancies, offsets, offset_dirs_count);
 }
 
 Board king_legal_moves(Position pos, GameState* gs) {
@@ -637,15 +840,41 @@ Board king_legal_moves(Position pos, GameState* gs) {
     return moves;
 }
 
-Board rook_attacks(Position pos, GameState* gs) {
-    const PosOffset offsets[4] = {
-        { 1, 0 },
-        { -1, 0 },
-        { 0, 1 },
-        { 0, -1 },
-    };
+static PosOffset ROOK_OFFSETS[4] = {
+    { 1, 0 },
+    { -1, 0 },
+    { 0, 1 },
+    { 0, -1 },
+};
 
-    return sliding_piece_attacks(pos, gs, offsets, 4);
+const PosOffset BISHOP_OFFSETS[4] = {
+    { 1, 1 },
+    { -1, 1 },
+    { 1, -1 },
+    { -1, -1 },
+};
+
+u64 magic_board_hash(Board occupancies, u64 moves_mask, u64 magic, int shift) {
+    occupancies &= moves_mask;
+    occupancies *= magic;
+    occupancies >>= 64 - shift;
+    return occupancies;
+}
+
+Board magic_rook_attacks(Position pos, GameState* gs) {
+    Board occupancies = gs->all_pieces &
+        ~gs->side[!gs->turn].piece[P_KING];
+
+    u64 hash = magic_board_hash(occupancies, ROOK_MOVES_LUT[pos],
+        ROOK_MAGIC_NUMBERS[pos], ROOK_SHIFT);
+
+    assert(hash < ROOK_PERMS);
+    return ROOK_MAGIC_BOARDS[pos][hash];
+}
+
+Board rook_attacks(Position pos, GameState* gs) {
+    // return sliding_piece_attacks_from_gs(pos, gs, ROOK_OFFSETS, 4);
+    return magic_rook_attacks(pos, gs);
 }
 
 Board rook_legal_moves(Position pos, GameState* gs) {
@@ -665,16 +894,26 @@ Board rook_legal_moves(Position pos, GameState* gs) {
     return moves;
 }
 
-Board bishop_attacks(Position pos, GameState* gs) {
-    const PosOffset offsets[4] = {
-        { 1, 1 },
-        { -1, 1 },
-        { 1, -1 },
-        { -1, -1 },
-    };
+Board magic_bishop_attacks(Position pos, GameState* gs) {
+    // King is excluded because it is impossible to block
+    // with the king and otherwise the king can just move
+    // backwards and be out of check as this attack path
+    // will go up to the king and stop if he is not excluded.
+    Board occupancies = gs->all_pieces &
+        ~gs->side[!gs->turn].piece[P_KING];
 
-    return sliding_piece_attacks(pos, gs, offsets, 4);
+    u64 hash = magic_board_hash(occupancies, BISHOP_MOVES_LUT[pos],
+        BISHOP_MAGIC_NUMBERS[pos], BISHOP_SHIFT);
+
+    assert(hash < BISHOP_PERMS);
+    return BISHOP_MAGIC_BOARDS[pos][hash];
 }
+
+Board bishop_attacks(Position pos, GameState* gs) {
+    return magic_bishop_attacks(pos, gs);
+    // return sliding_piece_attacks_from_gs(pos, gs, BISHOP_OFFSETS, 4);
+}
+
 
 Board bishop_legal_moves(Position pos, GameState* gs) {
     assert_piece_exists(P_BISHOP, pos, gs);
@@ -954,17 +1193,21 @@ Board check_if_piece_pins(GameState* gs,
             if ((dir + 1) % 2 == 0) {
                 return pos_to_bitboard(blocker_pos);
             }
+            break;
         }
         case P_BISHOP: {
             if (dir % 2 == 0) {
                 return pos_to_bitboard(blocker_pos);
             }
+            break;
         }
         case P_QUEEN: {
             return pos_to_bitboard(blocker_pos);
         }
         default: return 0;
     }
+
+    return 0;
 }
 
 void get_pins(GameState* gs, SideId side) {
@@ -1157,7 +1400,7 @@ Board check_attack_path(GameState* gs, SideId s) {
 
                     const PosOffset offsets[1] = { direction(king_pos, pos) };
 
-                    b |= sliding_piece_attacks(king_pos, gs, offsets, 1);
+                    b |= sliding_piece_attacks_from_gs(king_pos, gs, offsets, 1);
                 }
             }
         }
@@ -1166,9 +1409,7 @@ Board check_attack_path(GameState* gs, SideId s) {
     return b;
 }
 
-static Board COLS[8];
-
-float material_eval(GameState* gs) {
+float evaluate_material(GameState* gs) {
     float eval = 0;
 
     for (Piece p = 0; p < 6; p++) {
@@ -1250,7 +1491,7 @@ float evaluate(GameState* gs) {
     float absolute_pins = set_bits_count(gs->side[S_BLACK].pinned_pieces)
         - set_bits_count(gs->side[S_WHITE].pinned_pieces);
 
-    return material_eval(gs) * 1.5
+    return evaluate_material(gs) * 1.5
         + castle_rights * 0.4
         + checks * 1.2
         + absolute_pins * 0.9
@@ -1420,6 +1661,8 @@ GameState parse_FEN(char* fen) {
         exit(1);
     }
 
+    update_game_state_info(&gs);
+
     return gs;
 }
 
@@ -1469,26 +1712,22 @@ void update_game_state_info(GameState* gs) {
     (void)get_pins(gs, S_WHITE);
     (void)get_pins(gs, S_BLACK);
 
-    if (gs->side[!gs->turn].in_check) {
-        // Checkmate
-        MoveList ml = { 0 };
-        Move moves[128];
-        ml.moves = moves;
+    MoveList ml = { 0 };
+    Move moves[128];
+    ml.moves = moves;
 
-        get_all_legal_moves(gs, &ml);
+    // Turn needs to be inverted; we are seeing if the opponent
+    // will have any legal moves on the next move.
+    gs->turn = !gs->turn;
+    get_all_legal_moves(gs, &ml);
+    gs->turn = !gs->turn;
 
-        if (ml.length == 0) {
+    if (ml.length == 0) {
+        if (gs->side[!gs->turn].in_check) {
+            // Checkmate
             gs->game_result = (!gs->turn) == S_WHITE ? R_BLACK_WON : R_WHITE_WON;
-        }
-    } else {
-        // Stalemate
-        MoveList ml = { 0 };
-        Move moves[128];
-        ml.moves = moves;
-
-        get_all_legal_moves(gs, &ml);
-
-        if (ml.length == 0) {
+        } else {
+            // Stalemate
             gs->game_result = R_DRAW;
         }
     }
@@ -1581,10 +1820,110 @@ void generate_cols() {
     }
 }
 
+void generate_rows() {
+    for (int i = 0; i < 8; i++) {
+        ROWS[i] = 0xFFUL << ((u64)i * 8UL);
+    }
+}
+
+void occupancy_permutations(Board mask, Board* buf, int* buf_len, int buf_capacity) {
+    PositionList pl = { 0 };
+    Position positions[64];
+    pl.positions = positions;
+
+    bitboard_to_position_list(mask, &pl);
+
+    int permutations = 1 << pl.length;
+
+    assert(buf_capacity >= permutations);
+
+    *buf_len = permutations;
+
+    for (u64 perm = 0; perm < permutations; perm++) {
+        Board b = 0;
+        for (int pos_index = 0; pos_index < pl.length; pos_index++) {
+            if ((perm & (1UL << pos_index)) != 0) {
+                b |= pos_to_bitboard(pl.positions[pos_index]);
+            }
+        }
+        buf[perm] = b;
+    }
+}
+
+void generate_magic_bitboards(Board* magic_boards, int hash_space, u64* magic_numbers, Board* moves_lut,
+    PosOffset* offsets, int offsets_len, int shift) {
+
+    for (Position pos = 0; pos < 64; pos++) {
+        #define MAX_PERMS 16384
+        Board permutations[MAX_PERMS] = { 0 };
+        int permutations_len = 0;
+
+        Board mask = moves_lut[pos];
+
+        occupancy_permutations(mask, permutations,
+            &permutations_len, MAX_PERMS);
+
+        Board attacks[MAX_PERMS] = { 0 };
+
+        for (int i = 0; i < MAX_PERMS; i++) {
+            Board occupancies = permutations[i];
+            Board attacked_squares = sliding_piece_attacks(pos, occupancies, offsets, offsets_len);
+            attacks[i] = attacked_squares;
+        }
+
+        retry_with_new_magic:
+
+        for (int i = 0; i < hash_space; i++) {
+            magic_boards[pos * hash_space + i] = 0;
+        }
+
+        for (int perm = 0; perm < permutations_len; perm++) {
+            Board occupancies = permutations[perm];
+            Board attacked_squares = attacks[perm];
+
+            u64 magic = magic_numbers[pos];
+            u64 hash = magic_board_hash(occupancies, mask, magic, shift);
+            assert(hash < hash_space);
+
+            Board current_value = magic_boards[pos * hash_space + hash];
+
+            if (current_value != 0 && current_value != attacked_squares) {
+                #ifndef GENERATE_MAGICS
+                    printf("Pos: %d has an insufficient magic number (%lu).\n", pos, magic);
+                    assert(false && "Hash collision!.");
+                #else
+                    magic_numbers[pos] = rand();
+                    magic_numbers[pos] <<= 32;
+                    magic_numbers[pos] |= rand();
+
+                    goto retry_with_new_magic;
+                #endif
+            }
+
+            magic_boards[pos * hash_space + hash] = attacked_squares;
+        }
+        #ifdef GENERATE_MAGICS
+            printf("%lu,\n", magic_numbers[pos]);
+        #endif
+    }
+}
+
 void generate_LUTs() {
-    generate_knight_LUT();
-    generate_king_LUT();
-    generate_cols();
+    (void)generate_knight_LUT();
+    (void)generate_king_LUT();
+
+    (void)generate_cols();
+    (void)generate_rows();
+
+    (void)generate_rook_LUT();
+    (void)generate_bishop_LUT();
+
+    (void)generate_magic_bitboards((Board*)ROOK_MAGIC_BOARDS, ROOK_PERMS, ROOK_MAGIC_NUMBERS,
+        ROOK_MOVES_LUT, ROOK_OFFSETS, 4, ROOK_SHIFT);
+
+    (void)generate_magic_bitboards((Board*)BISHOP_MAGIC_BOARDS, BISHOP_PERMS, BISHOP_MAGIC_NUMBERS,
+        BISHOP_MOVES_LUT, (PosOffset*)BISHOP_OFFSETS, 4, BISHOP_SHIFT);
+    // (void)generate_magic_bishop();
 }
 
 void set_up_board(GameState* gs) {
@@ -1757,7 +2096,7 @@ EvaluatedMove minimax(int depth, GameState* gs, float alpha, float beta, Transpo
 }
 
 EvaluatedMove engine_move(GameState* gs, TranspositionTable* tt) {
-    EvaluatedMove em = minimax(5, gs, -INFINITY, INFINITY, tt);
+    EvaluatedMove em = minimax(6, gs, -INFINITY, INFINITY, tt);
 
     if (is_null_move(&em.move)) {
         // printf("%s has forced mate!\n", em.eval > 0 ? "White" : "Black");
@@ -1767,7 +2106,6 @@ EvaluatedMove engine_move(GameState* gs, TranspositionTable* tt) {
         ml.moves = moves;
         get_all_legal_moves(gs, &ml);
         if (ml.length == 0) {
-            // printf("Stalemate!\n");
             return (EvaluatedMove) { 0 };
         }
         em.move = ml.moves[0];
@@ -1793,7 +2131,7 @@ void play_self_loop(GameState* gs, TranspositionTable* tt) {
         move_to_notation(em.move, move_notation, side_to_move);
 
         if (is_null_move(&em.move)) {
-            printf("Stalemate!\n");
+            printf("Null move!");
             break;
         }
 
@@ -1892,9 +2230,13 @@ void UCI_loop(GameState* gs, TranspositionTable* tt) {
 }
 
 int main(int argc, char* argv[]) {
+    srand(0xbadd);
+
     GameState gs = { 0 };
 
     (void)generate_LUTs();
+
+    printf("Initialised.\n");
 
     (void)set_up_board(&gs);
 
@@ -1905,13 +2247,10 @@ int main(int argc, char* argv[]) {
 
     #define move(m) apply_move(&gs, notation_to_move(m, &gs))
 
-    // FIXME: This causes a segfault when you try to calculate the next move.
-    // gs = parse_FEN("Knnnkbnn/pppppnpp/n1nnnnnn/nqnnrQnn/nnbNPpnn/nnnnnnPn/PPPPnPnP/RNB2B1R b A e3 0 1");
-
     // GameState gs_ = parse_FEN("2r1nrk1/p2q1ppp/bp1p4/n1pPp3/P1P1P3/2PBB1N1/4QPPP/R4RK1 w - - bm f4");
     // update_all_piece_stores(&gs_);
 
-    // play_self_loop(&gs_, &tt);
+    // play_self_loop(&gs, &tt);
     UCI_loop(&gs, &tt);
 
     // FIXME: position startpos moves g2g3 c7c6 f1g2 d7d5 g1f3 h7h6 e1g1 h8h7 d2d4 c8f5 b1c3 f7f6 c1f4 g7g5 f4e3 h7g7 d1d2 d8b6 a1d1 a7a6 b2b3 h6h5 h2h4 g5h4 g3h4 f5h3 e3g5 h3g2 f1e1 g2f3 g1f1 f3e4 g5f4 e4g2 f1g1 g2e4 g1h2 b6d8 f4h6 g8h6 d2h6 d8d6 h6f4 a6a5 e2e3 e4c2 d1d2 g7g4 f4d6
@@ -1920,6 +2259,8 @@ int main(int argc, char* argv[]) {
     // FIXME: position startpos moves e2e4 e7e6 d2d4 g7g6 g1f3 b8c6 b1c3 f8b4 f1b5 a7a6 b5c6 b7c6 c1d2 h7h6 e1g1 b4a5 d2f4 h8h7 d4d5 a5c3 b2c3 e8f8 d1d4 g6g5 f4e5 d7d6 d5c6 g5g4 f3h4 d6e5 d4e5 d8h4 e5c5 h4e7 c5b4 f8g7 a1e1 e7b4 c3b4 a8b8 c2c3 a6a5 a2a3 c8a6 c3c4 a6c4 e1e3 c4f1 g1f1 b8b4 e3g3 b4e4 f2f3 g3f3 e4e5 f3f4
     // 2023-10-15 21:32:35.094-->1:go infinite
     // 2023-10-15 21:32:35.096<--1:engine: main.c:394: void assert_piece_exists(Piece, Position, GameState *): Assertion `gs->side[gs->turn].piece[p] & pos_to_bitboard(pos)' failed.
+
+    // FIXME: position startpos moves g2g3 c7c6 f1g2 d7d5 g1f3 h7h6 e1g1 h8h7 d2d4 c8f5 c1f4 g7g5 f3h4 g5f4 h4f5 e7e6 b1c3 e6f5 d1d3 d8d6 d3f5 d6d8 f5h7 d8c7 h7g8 e8e7 g8h7 b7b5 e2e4 b5b4 c3e2 d5e4 e2f4 f8g7 h7g7 b8a6 g7h6 a8g8 h6h7 c7b8 a1e1 b8b6 e1e4 e7d8 h7g8 d8c7 g8f7
 
     return 0;
 }
