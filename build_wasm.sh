9d8fe4e19bc21add05f81b30d52438858eb10ee6
Jasper <40232406+jaspwr@users.noreply.github.com>
2024-01-23 22:15:59 +1100

    add readme

--- build_wasm.sh
+++ build_wasm.sh
@@ -4,7 +4,7 @@
 ./build.sh
 
 # WASM build
-emcc *.c --no-entry -s TOTAL_MEMORY=33685504 -o engine.mjs \
+emcc -DWASM *.c --no-entry -s TOTAL_MEMORY=33685504 -o engine.mjs \
     -s EXPORTED_FUNCTIONS='["_WASM_init", "_WASM_generate_move", "_WASM_import_game_state", "_WASM_make_move"]' \
     -s EXPORTED_RUNTIME_METHODS='["ccall", "cwrap"]' \
     --pre-js wasm/locateFile.js  \
